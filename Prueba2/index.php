fe457564f408899ed46896e0fd9988cef3793679
Nayden Naydenov <113701583+Nayden4@users.noreply.github.com>
2025-02-16 15:02:39 +0100

    terminal echos info

--- Prueba2/index.php
+++ Prueba2/index.php
@@ -6,14 +6,23 @@ $total = 75;  // Número total de imágenes a obtener
 $n = 1;  // Contador
 
 // Abre el archivo CSV para agregar la información
+echo "Abriendo el archivo CSV...\n";
+
 openCsv();
 
 while ($n <= $total) {
+
+    echo "Obteniendo la imagen $n de $total \n";
+
     $imageInfo = getRandomImage();  // Obtiene la información de la imagen
     writeCsv($imageInfo);  // Escribe la información de la imagen en el CSV
 
     $n++;
 }
 
+echo "Cerrando el archivo CSV...\n";
+
 // Cierra el archivo CSV
 closeCsv();
+
+echo "Proceso completado. Se han procesado $total imágenes.\n";
